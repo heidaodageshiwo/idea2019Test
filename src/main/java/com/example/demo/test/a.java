d6758110dc8f59cb830dfdbc1a9d8484a6062085
zhangqiang <heidaodageshiwo@163.com>
2019-11-16 20:02:29 +0800

    https://github.com/heidaodageshiwo/idea2019Test.git

---
+++ src/main/java/com/example/demo/test/a.java
@@ -0,0 +1,25 @@
+package com.example.demo.test;
+
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: demo
+ * @Package: com.example.demo.test
+ * @ClassName: a
+ * @Description: java类作用描述
+ * @Author: zhangq
+ * @CreateDate: 2019-11-16 19:51
+ * @UpdateUser: zhangq
+ * @UpdateDate: 2019-11-16 19:51
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ * *
+ */
+public class a {
+    int c=0;
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("a.main");
+    }
+}
